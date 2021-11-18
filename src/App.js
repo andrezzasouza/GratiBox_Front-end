4aeec9df397a8f9c18fddf51121730efc6bc6f26
Andrezza Souza <andrezzasouza@id.uff.br>
2021-11-18 00:58:52 -0300

    Set the basis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import GlobalStyle from "./assets/styles/GlobalStyle";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Plans from "./pages/Plans";
+import Details from "./pages/Details";
+import Subscription from "./pages/Subscription";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/plans" component={Plans} />
+        <Route exact path="/details" component={Details} />
+        <Route exact path="/subscription" component={Subscription} />
+      </Switch>
+    </Router>
   );
 }
 
