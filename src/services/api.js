ed9f35f6d70fa6e2f82725aec2ed72b756bd29eb
Andrezza Souza <72293497+andrezzasouza@users.noreply.github.com>
2025-11-30 15:57:36 -0300

    refactor: Replace hard-coded API URL with ENV variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://urgratiboxshere.herokuapp.com';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const createHeaders = (token) => ({
   headers: {
@@ -9,11 +9,16 @@ const createHeaders = (token) => ({
 });
 
 const signUp = (body) => axios.post(`${API_URL}/sign-up`, body);
+
 const login = (body) => axios.post(`${API_URL}/login`, body);
+
 const details = (token) =>
   axios.get(`${API_URL}/details`, createHeaders(token));
+
 const placeOrder = (body, token) =>
   axios.post(`${API_URL}/place-order`, body, createHeaders(token));
+
 const updateStorage = (token) =>
   axios.get(`${API_URL}/update`, createHeaders(token));
+
 export { signUp, login, placeOrder, details, updateStorage };
